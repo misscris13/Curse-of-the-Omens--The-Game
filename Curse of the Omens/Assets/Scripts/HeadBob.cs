b1b168e343574c6868c3991240bcacb65c98de3c
Cristal <cristalcamposabad@gmail.com>
2022-11-03 12:18:14 +0100

    D3 y D4: Añadido head bob
    Y cambios estéticos sin importancia.

---
+++ Curse of the Omens/Assets/Scripts/HeadBob.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBob : MonoBehaviour {
+    
+    Vector3 startPos;
+    public float amplitude = 1.7f;
+    public float period = 5f;
+    
+    protected void Start() {
+        startPos = transform.position;
+    }
+    
+    protected void Update() {
+        float theta = Time.timeSinceLevelLoad / period;
+        float distance = amplitude * Mathf.Sin(theta);
+        transform.position = startPos + Vector3.up * distance + Vector3.right * ((distance+2)/2);
+    }
+}
