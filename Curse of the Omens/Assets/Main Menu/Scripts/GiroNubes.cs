b0ff820fcddb3e4f37713f8c267ee43db2150a1c
Cristal <cristalcamposabad@gmail.com>
2023-01-31 11:42:10 +0100

    Menú principal: mejoras
    Nubes y giro, skybox, texturas

---
+++ Curse of the Omens/Assets/Main Menu/Scripts/GiroNubes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroNubes : MonoBehaviour
+{
+    public float vel = 0.01f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0.0f, vel, 0.0f, Space.Self);
+    }
+}
