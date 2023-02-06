0cfefc3c8b0082b7ceffacb516ad0bce49c68442
Cristal <cristalcamposabad@gmail.com>
2023-02-06 12:14:51 +0100

    Combat: init

---
+++ Curse of the Omens/Assets/Combat/Scripts/CombatManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player playerObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // recover player stats like ult points, hp and mana points
+        // just for testing:
+        playerObject.hp = 50;
+        playerObject.mana = 50;
+        playerObject.ult = 0;
+        playerObject.maxUlt = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void manageAttack (Entity dealer, Entity target, string dmgType, float dmg, bool isPlayer)
+    {
+        //target.Damage(dmg);
+
+        if (dealer.ult < dealer.maxUlt)
+        {
+            dealer.ult++;
+        }
+        
+        //target.Damage(dmgType, dmg);
+    }
+}
---
+++ Curse of the Omens/Assets/Combat/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float hp;
+    public float mana;
+    public int ult;
+    public int maxUlt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
